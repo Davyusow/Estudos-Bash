fbabe629b60bc5afd7ad410c19f006ce9d072e62
Davyusow <dayvsonf27@gmail.com>
2025-12-22 15:16:27 -0300

    Adição inicial da sintaxe das expressões regulares

--- Aula 09/expregulares.sh
+++ Aula 09/expregulares.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/bin/fish
 
 cat ../template.txt | grep cat
 cat ../template.txt | grep ^cat #retorna todas as vezes que uma palavra começa com cat
-cat ../template.txt | grep cat$ #retorna todas as vezes que uma palavra termina com cat
+#cat ../template.txt | grep cat$ #retorna todas as vezes que uma palavra termina com cat
 
 echo "#####"
+
+cat ../template.txt | grep .e
+# . é todas as vezes que tiver um caractere seguido de dele no caso *e
+echo "#####"
+cat ../template.txt | grep [[:upper:]] # upper retorna apenas caracteres maiusculus
